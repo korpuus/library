e300c43878be22e85c9237f9e4dd29687c2d397b
korpuus <korpus.kuba@gmail.com>
2024-05-22 20:13:28 +0200

    refactor: Convert Book constructor to ES6 class syntax

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const myLibrary = [];
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-function Book(title, author, pages, read) {
-  this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.read = read,
-  this.info = function () { return (`"${title}" by ${author}, ${pages} pages`);}
+  info() {
+    return (`"${this.title}" by ${this.author}, ${this.pages} pages`);
+  }
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'yes');
